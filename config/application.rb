146f6723e86367976374ee7610bb8190ddbd83d9
Andrey Antonov <aa@oldtimer-info.de>
2011-05-17 10:05:11 +0200

    Finished layout and routes

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,11 @@ module SampleApp
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.generators.helper = false
+    config.view_specs = false
+    config.helper_specs = false
+
+
   end
 end
